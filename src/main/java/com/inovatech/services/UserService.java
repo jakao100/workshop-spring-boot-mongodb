aa353a6fafc9ed42efcb8d267a82c42440430fce
Jakson Albququerque <ipcamexterno@gmail.com>
2025-04-02 15:27:22 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/inovatech/services/UserService.java
@@ -0,0 +1,23 @@
+package com.inovatech.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inovatech.domain.User;
+import com.inovatech.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		
+		return repo.findAll();
+		
+	}
+
+}
